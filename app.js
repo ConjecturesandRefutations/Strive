d52df115504f6a9845b0acec367c273f0ffdf950
cursedxp <anilozsoy@gmail.com>
2022-11-28 20:46:11 +0100

    Authentication config has been created

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
+//Authentication config File
+require("./config/auth.config")(app);
+
 // default value for title local
 const capitalize = require("./utils/capitalize");
 const projectName = "Strive";

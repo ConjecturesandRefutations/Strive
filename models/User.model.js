c6206d07dcafdcfc68d5f796c25541ff3dcb8813
cursedxp <anilozsoy@gmail.com>
2022-12-01 19:50:19 +0100

    Registering new user to the db

--- models/User.model.js
+++ models/User.model.js
@@ -1,38 +1,40 @@
-const { Schema, model } = require("mongoose");
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
 const userSchema = new Schema(
   {
     name: String,
     age: Number,
-    favoriteActivites: { type: String, enum: ['run', 'bike', 'swim', 'walk', 'other'] },
+    favoriteActivites: {
+      type: String,
+      enum: ["run", "bike", "swim", "walk", "other"],
+    },
     shoes: [String],
     bikes: [String],
-    posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }]
-  },
-    {
-      username: {
-        type: String,
-        trim: true,
-        required: false,
-        unique: true
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
-      },
-      password: {
-        type: String,
-        required: true
-      }
+    profileImage: String,
+    posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    username: {
+      type: String,
+      trim: true,
+      required: false,
+      unique: true,
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
   {
-    timestamps: true
+    timestamps: true,
   }
 );
 
 const User = model("User", userSchema);
-
 module.exports = User;
--- routes/auth/register.routes.js
+++ routes/auth/register.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/User.model");
 
 /* GET register page */
 router.get("/register", (req, res, next) => {
@@ -8,12 +9,15 @@ router.get("/register", (req, res, next) => {
 
 /* POST register page*/
 
-router.post("/register" , (req, res, next) => {
+router.post("/register", (req, res, next) => {
   console.log("The form data: ", req.body);
 
- const { username, email, password } = req.body;
-
-
- })
+  const { username, email, password } = req.body;
+  User.create({ username: username, email: email, password: password }).then(
+    (result) => {
+      console.log("🧑🏻‍💻New user has been created");
+    }
+  );
+});
 
 module.exports = router;

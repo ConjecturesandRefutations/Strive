49566dc95c1077b4a5a62c44377082734bb1495e
cursedxp <anilozsoy@gmail.com>
2022-12-01 20:12:52 +0100

    Directing user to profile screen after registration

--- routes/auth/login.routes.js
+++ routes/auth/login.routes.js
@@ -1,26 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-const app = express()
+const app = express();
 
 /* GET login page */
 router.get("/login", (req, res, next) => {
   res.render("auth/login");
 });
 
-router.get("/login", (req, res, next) => {
-  res.render("auth/login");
-});
-
 router.post("/login", (req, res, next) => {
   const { username, password } = req.body;
-  console.log(req.body)
+  console.log(req.body);
 
   //check if we have all info
   if (!username || !password) {
     res.render("auth/login", { err: "please fill out all information" });
     return;
-  }})
-
+  }
+});
 
 module.exports = router;

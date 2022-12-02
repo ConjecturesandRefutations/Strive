afcdd5dbbce4537397ccdae8eec582dba030bbba
Alfie Collins <alfiecollins44@hotmail.co.uk>
2022-12-02 18:16:10 +0000

    adding posts to the database

--- routes/createActivity.routes.js
+++ routes/createActivity.routes.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
+const Post = require("../models/Post.model");
+
 const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard");
 
 
 router.get("/",  isLoggedIn,  (req, res, next) => {
   res.render("createActivity");
+});
+
+router.post("/", (req, res, next) => {
+  console.log(req.body);
+  const { title, distance, duration, elevation, description } = req.body;
+
+  Post.create({ title, distance, duration, elevation, description })
+     .then(postFromDB => console.log(`New post created: ${postFromDB.title}.`))
+    .then(() => res.redirect("/profile"))
+    .catch((error) => next(error));
 });
 
 module.exports = router;

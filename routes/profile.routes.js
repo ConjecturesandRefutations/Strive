146f8a3a4b3cb6a42d429e08228aa66517db461d
Alfie Collins <alfiecollins44@hotmail.co.uk>
2022-12-06 20:21:49 +0000

    populating posts not working

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post.model");
+const User = require("../models/User.model");
 
 const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard");
 
 /* GET profile page */
-router.get("/profile", isLoggedIn, (req, res, next) => {
-  console.log(req.session.currentUser);
-  res.render("users/profile", { userInSession: req.session.currentUser });
+router.get("/profile", isLoggedIn, async (req, res, next) => {
+  User.findById(req.session.currentUser._id).populate('posts') 
+  .then(result => {res.render('users/profile', { posts: result.posts })}
+  )
+ /*  console.log(req.session.currentUser);
+  User.findById(req.session.currentUser._id).populate('posts') 
+  .then(foundUser => {
+       console.log(foundUser.posts)
+      res.render('users/profile', { posts: foundUser.posts });
+    })
+    .catch(err => {
+      console.log(`Err while getting the posts from the DB: ${err}`);
+      next(err);
+    }); */
 });
 
 module.exports = router;
+

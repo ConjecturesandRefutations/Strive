16349343bad4fcecfcc705b8d2fcc9befbf71765
Alfie Collins <alfiecollins44@hotmail.co.uk>
2022-12-06 21:59:02 +0000

    basic activities view, listing each activity

--- routes/myActivities.routes.js
+++ routes/myActivities.routes.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User.model"); //
 
 const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard");
 
 /* GET my activities page */
-router.get("/",  isLoggedIn,  (req, res, next) => {
-  res.render("myActivities");
+router.get('/', (req, res, next) => {
+  User.findById(req.session.currentUser)
+    .populate('posts') // 
+    .then(dbPosts => {
+      //console.log("Posts from the DB: ", dbPosts.posts);
+      res.render('myActivities', { posts: dbPosts.posts });
+    })
+    .catch(err => {
+      console.log(`Err while getting the posts from the DB: ${err}`);
+      next(err);
+    });
 });
 
 module.exports = router;

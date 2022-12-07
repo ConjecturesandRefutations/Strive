e77e5cd1df1e03bb0df17e84f7873906b164fef6
Alfie Collins <alfiecollins44@hotmail.co.uk>
2022-12-07 01:42:01 +0000

    removing a load of commented code

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -19,22 +19,5 @@ router.get('/profile', isLoggedIn, (req, res, next) => {
     });
 });
 
-/* GET profile page */
-//router.get("/profile", isLoggedIn, async (req, res, next) => {
-  //User.findById(req.session.currentUser._id).populate('posts') 
-  //.then(result => {res.render('users/profile', { posts: result.posts })}
-  //)
- /*  console.log(req.session.currentUser);
-  User.findById(req.session.currentUser._id).populate('posts') 
-  .then(foundUser => {
-       console.log(foundUser.posts)
-      res.render('users/profile', { posts: foundUser.posts });
-    })
-    .catch(err => {
-      console.log(`Err while getting the posts from the DB: ${err}`);
-      next(err);
-    }); */
-//});
-
 module.exports = router;
 

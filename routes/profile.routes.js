6bd0b14b8f113f78b32fa9ed366c132e1897df86
Anil Ozsoy <anilozsoy@gmail.com>
2022-12-08 12:06:14 +0100

    post check for new users

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -14,32 +14,42 @@ router.get("/profile", isLoggedIn, (req, res, next) => {
     .populate("posts") //
     .then((user) => {
       //console.log("Posts from the DB: ", dbPosts.posts);
-      user.posts.forEach((post) => {
-        distances.push(post.distance);
-        durations.push(post.duration);
-        elevations.push(post.elevation);
-      });
+      if (user.posts.length === 0) {
+        res.render("users/profile", {
+          posts: user.posts,
+          user: user,
+          sumDistances: 0,
+          sumDurations: 0,
+          sumElevations: 0,
+        });
+      } else {
+        user.posts.forEach((post) => {
+          distances.push(post.distance);
+          durations.push(post.duration);
+          elevations.push(post.elevation);
+        });
 
-      //this is not a perfect solution to find out sums but ok for demonstration
-      const sumDistance = distances.reduce((acc, curr) => {
-        return acc + curr;
-      });
+        //this is not a perfect solution to find out sums but ok for demonstration
 
-      const sumDurations = durations.reduce((acc, curr) => {
-        return acc + curr;
-      });
+        const sumDistances = distances.reduce((acc, curr) => {
+          return acc + curr;
+        });
 
-      const sumElevations = elevations.reduce((acc, curr) => {
-        return acc + curr;
-      });
+        const sumDurations = durations.reduce((acc, curr) => {
+          return acc + curr;
+        });
 
-      res.render("users/profile", {
-        posts: user.posts,
-        user: user,
-        sumDistances: sumDistance,
-        sumDurations: sumDurations,
-        sumElevations: sumElevations,
-      });
+        const sumElevations = elevations.reduce((acc, curr) => {
+          return acc + curr;
+        });
+        res.render("users/profile", {
+          posts: user.posts,
+          user: user,
+          sumDistances: sumDistances,
+          sumDurations: sumDurations,
+          sumElevations: sumElevations,
+        });
+      }
     })
     .catch((err) => {
       console.log(`Err while getting the posts from the DB: ${err}`);
